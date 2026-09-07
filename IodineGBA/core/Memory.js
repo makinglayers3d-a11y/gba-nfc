2596e9c0a7d23c9f42591555cdcaaa9f2b87cdf5
makinglayers3d-a11y <makinglayers3d@gmail.com>
2026-09-08 00:14:02 +0200

    Update Memory.js

--- IodineGBA/core/Memory.js
+++ IodineGBA/core/Memory.js
@@ -4088,25 +4088,10 @@ GameBoyAdvanceMemory.prototype.readUnused32MultiBase = function () {
     return this.IOCore.getCurrentFetchValue() | 0;
 }
 GameBoyAdvanceMemory.prototype.loadBIOS = function () {
-    /*
-     * Arranque sin BIOS cuando SKIPBoot está activado.
-     * El CPU hará su inicialización directa.
-     */
     if (this.IOCore.SKIPBoot) {
-        for (
-            var index = 0;
-            (index | 0) < 0x4000;
-            index = ((index | 0) + 1) | 0
-        ) {
-            this.BIOS[index & 0x3FFF] = 0;
-        }
-
         return 1;
     }
 
-    /*
-     * Arranque normal: exigir BIOS válida de 16 KiB.
-     */
     if ((this.IOCore.BIOS.length | 0) == 0x4000) {
         for (
             var index = 0;
@@ -4122,6 +4107,7 @@ GameBoyAdvanceMemory.prototype.loadBIOS = function () {
 
     throw(new Error("BIOS invalid."));
 }
+
     //Generic memory read dispatch generator:
     function compileMemoryReadDispatch(readUnused, readExternalWRAM, readInternalWRAM,
                                        readIODispatch, readVRAM, readROM, readROM2, readSRAM, readBIOS) {
